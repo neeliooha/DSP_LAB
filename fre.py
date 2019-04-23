9d3b4f94b43eab674372b5925ac5a92e786fd894
neeliooha <49877619+neeliooha@users.noreply.github.com>
2019-04-23 08:44:54 +0530

    Add files via upload

---
+++ fre.py
@@ -0,0 +1,17 @@
+import numpy as np
+from matplotlib import pyplot as plt
+t=np.pi
+T=0.1
+p=(0.35*t)
+c=(0.7*t)
+sp=0.6
+ss=0.1
+a=((2.0/T)*np.tan(p/2.0))
+b=((2.0/T)*np.tan(c/2.0))
+print a
+print b
+
+
+
+
+
--- hpf.py
+++ hpf.py
@@ -1,51 +1,89 @@
-#hpf using butterworth approximation
-import numpy as np 
-import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import pyplot as plt
 import cmath as c1
 T=0.1
+wc=0.35*np.pi
 wp=0.7*np.pi
-ws=0.35*np.pi
-def frequency(wp,ws):
-	ap=((2.0/T)*np.tan(wp/2.0))
-	ast=((2.0/T)*np.tan(ws/2.0))
-	return(ap,ast)
-t=frequency(wp,ws)
+def frequency(wp,wc):	
+	ap=((2.0/T)*np.tan(wp/2.0))	
+	ast=((2.0/T)*np.tan(wc/2.0))
+	return ap,ast
+t=frequency(wp,wc)
 ap=t[0]
-print "pass band frequency:",ap
+print "analog pass band edge fre=", ap
 ast=t[1]
-print "stop band frequency:",ast
+print "analog stop band edge fre=", ast
+
+
+
+
+
 
 #order
-adp=0.6
-ads=0.1
-x1=adp**2
-x2=ads**2
-app=1
-astt=ap/ast
-def order(x1,x2,app,ast):
-	m=((1.0/x1)-1.0)
-	n=((1.0/x2)-1.0)
-	x=m/n
-	y=np.sqrt(x)
-	z=np.log(y)
-	r=np.log(app/ast)
-	N=z/r
+sp=0.6
+ss=0.1
+h=ss**2
+i=sp**2
+
+def order(h,i,ap,ast):
+	x1=((1.0/h)-1.0)
+	x2=((1.0/i)-1.0)
+	q=(x1/x2)
+	k=((1.0/2.0)*np.log(q))
+	ap1=1
+	ast1=(ap/ast)
+	n=np.log(ast1/ap1)
+	N=(k/n)
 	return N
-y=order(x1,x2,app,ast)
+y=order (h,i,ap,ast)
 print y
-N1=np.ceil(y)	
-print "order=",N1
+m=np.abs(y)
+n1=np.ceil(m)
+print "order=",n1
+
+
+
+#cutt off frequency
+ss=0.1
+def cutof(n1,ss,ast,ap):
+	a1=((1.0/ss**2)-1.0)
+	a2=(1/(2.0*n1))
+	a3=(a1)**a2
+	f=((ap/ast)/a3)
+	return f
+e=cutof(n1,ss,ast,ap)
+print "cutt of frequency=",e	
+
+
+#trasfer function
+
+def trans(e,T,n1):
+	bk=2*np.sin((np.pi)/4.0)
+	j=c1.sqrt(-1)
+	w=np.arange(0,np.pi,0.01)
+	z=np.exp(j*w)
+	s=((2.0/T)*((1-(1/z))/(1+(1/z))))
+	s1=(ap/s)
+	p1=((s1**2)+(bk*e*s1)+(e**2))
+	p2=e**n1
+	hs1=(p2/p1)
+	return hs1
+h1=trans(e,T,n1)
+print"transfer function=",h1
+w=np.arange(0,np.pi,0.01)
+plt.plot(w,np.abs(h1))
+plt.show()
 
 
 
 
 
 
-	
 
 
 
-	
+
+
 
 
 
